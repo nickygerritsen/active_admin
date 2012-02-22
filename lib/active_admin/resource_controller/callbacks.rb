cb83f98b93b74ea987b640183220ef8ed443aa2f
Nicky Gerritsen <nickygerritsen@me.com>
2012-02-22 18:37:56 +0100

    with_role

--- lib/active_admin/resource_controller/callbacks.rb
+++ lib/active_admin/resource_controller/callbacks.rb
@@ -12,30 +12,37 @@ module ActiveAdmin
       protected
 
       def build_resource
+        with_role :admin
         object = super
         run_build_callbacks object
         object
+        with_role nil
       end
 
       def create_resource(object)
+        with_role :admin
         run_create_callbacks object do
           save_resource(object)
         end
+        with_role nil
       end
 
       def save_resource(object)
+        with_role :admin
         run_save_callbacks object do
           object.save
         end
+        with_role nil
       end
 
       def update_resource(object, attributes)
+        with_role :admin
         # Inherited resources 1.3.0 calls the method passing an array instead of a parameters hash.
         # The array contains in the first position the parameters which update the object and in the second position
         # possibly contains the role, it's optional, to use in mass assignment. This feature is provided by Rails 3.1
         if attributes.is_a?(Array)
           if object.respond_to?(:assign_attributes)
-            object.assign_attributes(*attributes, as: :admin)
+            object.assign_attributes(*attributes)
           else
             object.attributes = attributes[0]
           end
@@ -46,12 +53,15 @@ module ActiveAdmin
         run_update_callbacks object do
           save_resource(object)
         end
+        with_role nil
       end
 
       def destroy_resource(object)
+        with_role :admin
         run_destroy_callbacks object do
           object.destroy
         end
+        with_role nil
       end
     end
 
